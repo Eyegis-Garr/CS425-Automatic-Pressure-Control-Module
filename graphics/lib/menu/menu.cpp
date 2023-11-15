85b2a1c4fcf1035b2388c3a7f985bd53cf343b61
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-11-14 20:10:54 -0800

    base menu commit
    implemented basic interaction, option redrawing for updating selection, and pretty hardcoded spacing/formatting

--- graphics/lib/menu/menu.cpp
+++ graphics/lib/menu/menu.cpp
@@ -1,36 +1,82 @@
 #include "menu.h"
 
-menu_t *new_menu(uint8_t num_options, char **options, font_t *font, uint8_t flags) {
+menu_t *new_menu(const char *title, uint8_t num_options, const char *options[], font_t *font, uint8_t flags) {
     menu_t *ret = (menu_t*) malloc(sizeof(menu_t));
 
+    if (title) 
+        ret->title = (char*) malloc(sizeof(char) * TITLE_LEN);
+    strncpy(ret->title, title, TITLE_LEN);
     ret->nopts = num_options;
     ret->opts = options;
+    ret->center = ORIGIN;
+    ret->w = 0;
+    ret->h = 0;
+    ret->opt_sp = 0;
     ret->cursor = 0;
+    ret->cur_color = 0x1234;
     ret->font = font;
-    ret->flags = flags;
 
-    ret->m_element = NULL;
-    ret->o_element = NULL;
-    ret->font = NULL;
     ret->cb = NULL;
-
-    ret->flags |= 1 << INIT;
 
     return ret;
 }
 
 int m_set_aabb(menu_t *m, vec2 tl, vec2 br) {
     if (!m) return -1;
-    if (!(m->flags & (1 << INIT))) return -1;
 
     m->w = br.x - tl.x;
     m->h = tl.y - br.y;
-    m->opt_sp = m->font->spacing * m->font->scale;
-    m->topleft.x = tl.x;
-    m->topleft.y = tl.y;
+    m->center.x = tl.x + (m->w / 2);
+    m->center.y = br.y + (m->h / 2);
 
-    m->m_element->pos.x = m->topleft.x + (m->w >> 1);
-    m->m_element->pos.y = m->topleft.y + (m->h >> 1);
+    if (m->font)
+        m->opt_sp = m->font->spacing * m->font->scale + 3;
+
+    return 0;
+}
+
+int m_interact(menu_t *m, int it) {
+    if (!m) return -1;
+
+    switch (it) {
+        case CURS_UP:
+            m->cursor = mod(m->cursor - 1, m->nopts);
+            m_opt_redraw(m, mod(m->cursor + 1, m->nopts));
+            break;
+        case CURS_DOWN:
+            m->cursor = mod(m->cursor + 1, m->nopts);
+            m_opt_redraw(m, mod(m->cursor - 1, m->nopts));
+            break;
+        case CURS_LEFT:
+            // move up level
+            break;
+        case CURS_RIGHT:
+            // same as select?
+            break;
+        case CURS_SELECT:
+            m->cb(m->cursor);
+            break;
+        default:
+            break;
+    }
+
+    return 0;
+}
+
+int m_opt_redraw(menu_t *m, int opt) {
+    if (!m) return -1;
+
+    uint16_t fcolor = m->font->color;
+    int div = (m->h - m->opt_sp) / (m->nopts + 1);
+    int ymax = m->center.y + (m->h / 2) - m->opt_sp;
+    vec2 pos = (vec2) { m->center.x, ymax };
+
+    m->font->color = m->cur_color;
+    pos.y = ymax - ((m->cursor + 1) * div);
+    f_draw(m->font, m->opts[m->cursor], strlen(m->opts[m->cursor]), pos);
+    m->font->color = fcolor;
+    pos.y = ymax - ((opt + 1) * div);
+    f_draw(m->font, m->opts[opt], strlen(m->opts[opt]), pos);
 
     return 0;
 }
@@ -38,17 +84,32 @@ int m_set_aabb(menu_t *m, vec2 tl, vec2 br) {
 int m_draw(menu_t *m) {
     if (!m) return -1;
 
-    int vitems = m->h / m->opt_sp;
-    m->m_element->pos.x = m->topleft.x + (m->w >> 1);
-    m->m_element->pos.y = m->topleft.y + (m->h >> 1);
-    m->o_element->pos.x = m->topleft.x + (m->w >> 1);
-    e_bake_matrix(m->m_element);
-    e_draw(m->m_element);
-    for (int i = 0; i < vitems; i += 1) {
-        m->o_element->pos.y = m->topleft.y + (i * m->opt_sp);
-        if (m->o_element->pos.y < m->topleft.y + m->h) {
-            e_bake_matrix(m->o_element);
-            e_draw(m->o_element);
+    uint16_t c;
+    int div = (m->h - m->opt_sp) / (m->nopts + 1);
+    int ymax = m->center.y + (m->h / 2) - m->opt_sp;
+    vec2 fpos = (vec2) { m->center.x, ymax };
+
+    // render menu frame
+    e_bake_matrix(&m->m_element);
+    e_draw(&m->m_element);
+
+    // render title
+    f_draw(m->font, m->title, strlen(m->title), fpos);
+
+    for (int i = 0; i < m->nopts + 1; i += 1) {
+        m->o_element.pos.y = ymax - (i * div);
+        e_bake_matrix(&m->o_element);
+        e_draw(&m->o_element);
+
+        fpos.y -= div;
+        if (i == m->cursor) {
+            c = m->font->color;
+            m->font->color = m->cur_color;
+            f_draw(m->font, m->opts[i], strlen(m->opts[i]), fpos);
+            m->font->color = c;
+
+        } else {
+            f_draw(m->font, m->opts[i], strlen(m->opts[i]), fpos);
         }
     }
 
--- graphics/lib/menu/menu.h
+++ graphics/lib/menu/menu.h
@@ -4,35 +4,57 @@
 #include <Arduino.h>
 #include "element.h"
 #include "font.h"
+#include "util.h"
 
 #define OPTION_LEN  32
+#define TITLE_LEN   16
 
-// FLAGS
-#define INIT        0
-#define SCROLLABLE  1
+// MENU INTERACTIONS
+#define CURS_UP     0
+#define CURS_DOWN   1
+#define CURS_RIGHT  2
+#define CURS_LEFT   3
+#define CURS_SELECT 4
+#define CURS_BACK   5
 
 typedef int (*MenuCallback)(int selection);
 
+/*
+TODO:
+    option abstraction
+    menu hierarchy abstraction
+    menu hierarchy configuration
+    menu hierarchy traversal
+    2D menu option traversal?
+        array of option lists/pairs
+    better color formatting?
+    menu draw clearing
+    menu freeing/pooling?
+*/
+
 typedef struct menu_t {
-    uint8_t flags;
+    char *title;
     uint8_t nopts;
-    char **opts;
+    const char **opts;
 
-    vec2 topleft;
-    uint8_t w;
-    uint8_t h;
-    uint8_t opt_sp;
+    vec2 center;
+    int w;
+    int h;
+    int opt_sp;
 
-    uint8_t cursor;
+    int cursor;
+    uint16_t cur_color;
 
-    element_t *m_element;
-    element_t *o_element;
+    element_t m_element;
+    element_t o_element;
     font_t *font;
     MenuCallback cb;
 };
 
-menu_t *new_menu(uint8_t num_options, char *options[OPTION_LEN], font_t *font, uint8_t flags);
+menu_t *new_menu(const char *title, uint8_t num_options, const char *options[], font_t *font, uint8_t flags);
 int m_set_aabb(menu_t *m, vec2 tl, vec2 br);
+int m_interact(menu_t *m, int it);
+int m_opt_redraw(menu_t *m, int opt);
 int m_draw(menu_t *m);
 
 #endif // MENU_H
