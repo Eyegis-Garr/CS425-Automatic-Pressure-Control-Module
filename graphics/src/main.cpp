81cfe23f1171607b733f0b860c4f74cb17de6c92
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-11-21 10:57:54 -0800

    modified to test set_option menus

--- graphics/src/main.cpp
+++ graphics/src/main.cpp
@@ -60,9 +60,7 @@ int build_rectangle(int width, int height, vertex_t *vbuf, int **ibuf, uint16_t 
 
 
 font_t amal;
-menu_t *main_menu, *presets, *config, *alarms, *pid;
-menu_t *pick_preset, *pick_param, *pick_mode, *pick_pid;
-menu_t *set_param;
+menu_t *main_menu, *set;
 element_t menu_box, option_box;
 state_t sys;
 
@@ -80,22 +78,12 @@ void init_menus() {
 
 	main_menu = new_menu("MAIN", 4, &amal, A_MENU, &menu_box, &option_box);
 	m_set_size(main_menu, 120, 210);
+    m_set_draw(main_menu, M_LIST);
 	main_menu->cur_color = 0x05A0;
 
-	pick_mode = new_menu("MODE", 3, &amal, A_MENU, &menu_box, &option_box);
-	m_set_size(pick_mode, 120, 210);
-
-	presets = new_menu("PRESET", 4, &amal, A_MENU, &menu_box, &option_box);
-	m_set_size(presets, 120, 210);
-
-	config = new_menu("CONFIG", 8, &amal, A_MENU, &menu_box, &option_box);
-	m_set_size(config, 120, 210);
-
-	pick_param = new_menu("PARAM", 6, &amal, A_MENU, &menu_box, &option_box);
-	m_set_size(pick_param, 120, 210);
-
-	pick_pid = new_menu("PID", 4, &amal, A_MENU, &menu_box, &option_box);
-	m_set_size(pick_pid, 120, 210);
+	set = new_menu("SET", 4, &amal, A_MENU, &menu_box, &option_box);
+    m_set_size(set, 120, 210);
+    m_set_draw(set, M_SET);
 
 	init_menu_options();
 }
@@ -104,51 +92,14 @@ void init_menu_options() {
 	option_t opts[8];
 
 	// MAIN MENU
-	opts[0] = (option_t) {"Mode", 	pick_mode,   0};
-	opts[1] = (option_t) {"Preset", presets, 	 0};
-	opts[2] = (option_t) {"Config", config,  	 0};
-	opts[3] = (option_t) {"Sleep", 	NULL, 		 0};
+	opts[0] = (option_t) {"Mode", 	set, 0};
+	opts[1] = (option_t) {"Preset", set, 0};
+	opts[2] = (option_t) {"Config", set, 0};
+	opts[3] = (option_t) {"Sleep", 	set, 0};
 	m_set_options(main_menu, main_menu->nopts, opts);
 
-	// PICK MODE
-	opts[0] = (option_t) {"Auto", 		NULL, 		0};
-	opts[1] = (option_t) {"Manual", 	NULL, 		0};
-	opts[2] = (option_t) {"EXIT", 		main_menu,  0};
-	m_set_options(pick_mode, pick_mode->nopts, opts);
-
-	// PRESETS
-	opts[0] = (option_t) {"Save", 	pick_preset, 0};
-	opts[1] = (option_t) {"Load", 	pick_preset, 0};
-	opts[2] = (option_t) {"Delete", pick_preset, 0};
-	opts[3] = (option_t) {"Exit", 	main_menu, 	 0};
-	m_set_options(presets, presets->nopts, opts);
-
-	// CONFIG
-	opts[0] = (option_t) {"MARX", 		pick_param, SC_MARX};
-	opts[1] = (option_t) {"MX-TG70", 	pick_param, SC_MARXTG70};
-	opts[2] = (option_t) {"MTG", 		pick_param, SC_MTG};
-	opts[3] = (option_t) {"SWITCH", 	pick_param, SC_SWITCH};
-	opts[4] = (option_t) {"SW-TG70", 	pick_param, SC_SWTG70};
-	opts[5] = (option_t) {"RECLAIM", 	set_param, 	SC_RECLAIM};		// only has configurable pressure
-	opts[6] = (option_t) {"MIN SUPL", 	set_param, 	SC_MINSUPL};		// only has configurable pressure
-	opts[7] = (option_t) {"Exit", 		main_menu, 	0};
-	m_set_options(config, config->nopts, opts);
-
-	// PICK PARAMETER
-	opts[0] = (option_t) {"Pressure", 	set_param, SP_PRESSURE};
-	opts[1] = (option_t) {"Purge", 		set_param, SP_TIME};
-	opts[2] = (option_t) {"Delay", 		set_param, SP_TIME};
-	opts[3] = (option_t) {"Alarm", 		set_param, SP_TIME};
-	opts[4] = (option_t) {"PID", 		pick_pid,  SP_PID};
-	opts[5] = (option_t) {"Exit", 		main_menu, 	0};
-	m_set_options(pick_param, pick_param->nopts, opts);
-
-	// PICK PID
-	opts[0] = (option_t) {"KP", set_param, SP_KP};
-	opts[1] = (option_t) {"KI", set_param, SP_KI};
-	opts[2] = (option_t) {"KD",	set_param, SP_KD};
-	opts[3] = (option_t) {"Exit", main_menu, 	0};
-	m_set_options(pick_pid, pick_pid->nopts, opts);
+    opts[0] = (option_t) {"00.00", NULL, 12.34};
+    m_set_options(set, set->nopts, opts);	
 }
 
 void setup() {
@@ -161,6 +112,8 @@ void setup() {
 	init_input();
 
 	m_draw(main_menu, 0);
+    set->center = B_MENU;
+    m_draw(set, 0);
 
 	sys.a_menu = main_menu;
 	sys.b_menu = NULL;
@@ -172,40 +125,40 @@ void setup() {
 
 int prev_cursor = -1;
 void loop() {
-	int input = get_input();
+	// int input = get_input();
 
-	if (input & U_BTN) {
-		m_interact(primary, CURS_UP);
-	} if (input & D_BTN) {
-		m_interact(primary, CURS_DOWN);
-	} if (input & R_BTN) {
-		if (secondary) {
-			m_draw(secondary, 1);
-			secondary->center = A_MENU;
-		}
-		secondary = primary->options[primary->cursor].target;
-		if (secondary) {
-			secondary->center = B_MENU;
-			m_draw(secondary, 0);
+	// if (input & U_BTN) {
+	// 	m_interact_default(primary, CURS_UP);
+	// } if (input & D_BTN) {
+	// 	m_interact_default(primary, CURS_DOWN);
+	// } if (input & R_BTN) {
+	// 	if (secondary) {
+	// 		m_draw(secondary, 1);
+	// 		secondary->center = A_MENU;
+	// 	}
+	// 	secondary = primary->options[primary->cursor].target;
+	// 	if (secondary) {
+	// 		secondary->center = B_MENU;
+	// 		m_draw(secondary, 0);
 			
-			hold = primary;
-			primary = secondary;
-		}
-	} if (input & L_BTN) {
-		if (secondary) {
-			m_draw(secondary, 1);
-			secondary->center = A_MENU;
+	// 		hold = primary;
+	// 		primary = secondary;
+	// 	}
+	// } if (input & L_BTN) {
+	// 	if (secondary) {
+	// 		m_draw(secondary, 1);
+	// 		secondary->center = A_MENU;
 			
-			primary = hold;
-			secondary = NULL;
-		}
-	} if (input & A_BTN) {
-		m_interact(primary, CURS_SELECT);
-	} if (input & B_BTN) {
-		m_interact(primary, CURS_BACK);
-	}
+	// 		primary = hold;
+	// 		secondary = NULL;
+	// 	}
+	// } if (input & A_BTN) {
+	// 	m_interact_default(primary, CURS_SELECT);
+	// } if (input & B_BTN) {
+	// 	m_interact_default(primary, CURS_BACK);
+	// }
 
-	prev_cursor = primary->cursor;
+	// prev_cursor = primary->cursor;
 }
 
 void init_input() {
@@ -214,30 +167,6 @@ void init_input() {
 
 int get_input() {
 	return *PIN_B;
-}
-
-int build_polygon(int n_vertices, vertex_t *vbuf, int **ibuf, int radius, uint16_t color) {
-	if (!vbuf || !ibuf || n_vertices <= 0) {
-		return -1;
-	}
-
-	(*ibuf) = (int*) malloc(sizeof(int) * n_vertices);
-	float r = DEG_TO_RAD * (360 / n_vertices);
-	mat3 o = IDENT_MAT3;
-	vec3 v = { 1, 1, 1 };
-
-	scale_mat3((vec3){radius, radius, 1}, &o);
-	rotate_mat3(r, &o);
-
-	for (int i = 0; i < n_vertices; i += 1) {
-		// compute color
-		vbuf[i].color = color;
-		transform_vec3(&o, &v, &vbuf[i].pos);
-		(*ibuf)[i] = i;
-		rotate_mat3(r, &o);
-	}
-
-	return 0;
 }
 
 int build_rectangle(int width, int height, vertex_t *vbuf, int **ibuf, uint16_t color) {
