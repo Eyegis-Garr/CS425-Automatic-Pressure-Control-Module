1194e05b153415d1ecc065e8003af7479d29edea
Sean Rolandelli <srolandelli@nevada.unr.edu>
2023-11-30 21:19:13 -0800

    working on how it will merge with main code

--- checksum.cpp
+++ checksum.cpp
@@ -138,6 +138,9 @@ int main()
 	double sent_message[] = {1.1, 3, 4.56};
 	double recv_message[] = {1.1, 2, 4.58};
 
+
+
+
     int array_length = sizeof(sent_message)/sizeof(*sent_message);
     std::cout << array_length << std::endl;
 
@@ -156,9 +159,167 @@ int main()
 }
 
 /*
-Things to think about or ask
 
-need to find if main code wants to pass in two values and check if they are the same, 
-or pass in one value and return checksum then have another function to check checksums
+	reclaimer saftey time is a thing in file writer and setup
+	but not file reader
+
+
+	convert array into checksum and compare to saved checksum
+
+
+put current settings into array
+
+
+	double currentSettings[45];
+
+
+    alarmEnable = previousSettingFile.readStringUntil('\n').toInt();
+	currentSettings[0] = alarmEnable;
+
+    isCouple = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[1] = isCouple;
+
+	Marxsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+	currentSettings[2] = Marxsetpoint;
+    
+	MTGsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+	currentSettings[3] = MTGsetpoint;
+    
+	Switchsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+	currentSettings[4] = Switchsetpoint;
+    
+	TG70Switchsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+	currentSettings[5] = TG70Switchsetpoint;
+    
+	TG70Marxsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[6] = TG70Marxsetpoint;
+
+	maxReclaimerPressure = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[7] = maxReclaimerPressure;
+
+	minReclaimerPressure = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[8] = minReclaimerPressure;
+
+	marxenableState = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[9] = marxenableState;
+	
+	mtgenableState = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[10] = mtgenableState;
+
+	switchenableState = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[11] = switchenableState;
+
+	tg70switchenableState = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[12] = tg70switchenableState;
+
+	tg70marxenableState = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[13] = tg70marxenableState;
+
+	marxmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[14] = marxmaxTime;
+ 
+	mtgmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[15] = mtgmaxTime;
+
+	switchmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[16] = switchmaxTime;
+
+	tg70switchmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[17] = tg70switchmaxTime
+
+	tg70marxmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[18] = tg70marxmaxTime
+
+	marxDelay = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[19] = marxDelay
+
+	mtgDelay = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[20] = mtgDelay
+
+	switchDelay = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[21] = switchDelay
+
+	tg70marxDelay = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[22] = tg70marxDelay;
+
+	tg70switchDelay = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[23] = tg70switchDelay;
+
+	marxPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[24] = marxPurgeTime;
+
+	mtgPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[25] = mtgPurgeTime;
+
+	switchPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[26] = switchPurgeTime;
+
+	tg70switchPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[27] = tg70switchPurgeTime;
+
+	tg70marxPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+    currentSettings[28] = tg70marxPurgeTime;
+
+	minBottlePressure = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[29] = minBottlePressure;
+
+	kp_Marx = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[30] = kp_Marx;
+
+	ki_Marx = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[31] = ki_Marx;
+
+	kd_Marx = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[32] = kd_Marx;
+
+	kp_MTG = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[33] = kp_MTG;
+
+	ki_MTG = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[34] = ki_MTG;
+
+	kd_MTG = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[35] = kd_MTG;
+
+	kp_Switch = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[36] = kp_Switch;
+
+	ki_Switch = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[37] = ki_Switch;
+
+	kd_Switch = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[38] = kd_Switch;
+
+	kp_SwitchTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[39] = kp_SwitchTG70
+
+	ki_SwitchTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[40] = ki_SwitchTG70;
+
+	kd_SwitchTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[41] = kd_SwitchTG70;
+
+	kp_MarxTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[42] = kp_MarxTG70;
+
+	ki_MarxTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+    currentSettings[43] = ki_MarxTG70;
+
+	kd_MarxTG70 = previousSettingFile.readStringUntil('\n').toDouble();	
+	currentSettings[44] = kd_MarxTG70;
+
+
+
+check integrity of data
+
+    for (int i = 0; i < 45; i++) {
+        if (checker(currentSettings[i], previousSettings[i], 8)) {
+            std::cout << "No Error" << std::endl;
+        }
+        else {
+            std::cout << "Error" << std::endl;
+        }
+    }
+
 
 */
