841f6e5d3fc34e7319cfaf8c56ecbf8dab896a44
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-12-03 13:41:09 -0800

    implemented set-menu interaction. refactored function signatures
    implemented basic 4 digit set-menu interaction. added TSPoint as parameter to interact routines. return values of interact routines could be more uniform (potentially) but for now we live with the jank

--- menu/lib/menu/menu_interact.cpp
+++ menu/lib/menu/menu_interact.cpp
@@ -7,89 +7,97 @@ MenuInteractCallback MENU_INTERACT[4] = {
     NULL
 };
 
-int m_interact(menu_t *m, int it) {
+int m_test_touch(TSPoint t, menu_t *m) {
     if (!m) return -1;
+    vec2 tpos = (vec2) { m->center.x, m->center.y - m->opt_offset - m->opt_div + 80 };
 
-    return MENU_INTERACT[m->it_flag](m, it);
+    for (int i = 0; i < m->nopts - 1; i += 2) {
+        if (TEST_RECT(t.x, t.y, A_COLUMN.x, tpos.y, COLUMN_W, COLUMN_H)) {        // CURRENT A OPTION
+            return i;
+        } else if (TEST_RECT(t.x, t.y, B_COLUMN.x, tpos.y, COLUMN_W, COLUMN_H)) { // CURRENT B OPTION
+            
+            return i + 1;
+        }
+
+        tpos.y += m->opt_div;
+    }
+
+    return -1;
 }
 
-int m_interact_default(menu_t *m, int it) {
-    static int prev_cursor;
-    if (!m) return -1;
+int m_test_touch_set(TSPoint t, menu_t *m) {
+    if (!m) return 0;
+    vec2 tpos = (vec2) { m->center.x, m->center.y - m->opt_offset - m->opt_div + 80 };
+    int cx_offset = CENTER.x - (S_BTN_WIDTH * 2);
+    int spacing, centering;
 
-    prev_cursor = m->cursor;
+    for (int i = 0; i < 4; i += 1) {
+        spacing = S_BTN_SPACING * i;
+        centering = cx_offset + (S_BTN_WIDTH * i);
+        if (TEST_RECT(t.x, t.y, centering + spacing, tpos.y,                        \
+            S_BTN_WIDTH, S_BTN_WIDTH)) {  // positive
+            return i + 1;
+        } else if (TEST_RECT(t.x, t.y, centering + spacing, tpos.y + (2 * m->opt_div), \
+            S_BTN_WIDTH, S_BTN_WIDTH)) {  // negative
+            return -1 * (i + 1);
+        }
+    }
 
-    switch (it) {
-        case CURS_UP:
-            m->cursor = mod(m->cursor - 1, m->nopts);
-            // if (prev_cursor < m->cursor) {  // page to bottom
-            //     m_list_options(m, 1);
-            //     m->coffset = m->cursor - (m->cursor % MAX_VIS_OPT);
-            //     m_list_options(m, 0);
-            // } else if (prev_cursor % MAX_VIS_OPT == 0) {  // page to previous
-            //     m_list_options(m, 1);
-            //     m->coffset -= MAX_VIS_OPT;
-            //     m_list_options(m, 0);
-            // }
-            break;
-        case CURS_DOWN:
-            m->cursor = mod(m->cursor + 1, m->nopts);
-            // if (m->cursor < prev_cursor) {  // page to top
-            //     m_list_options(m, 1);
-            //     m->coffset = 0;
-            //     m_list_options(m, 0);
-            // } else if (m->cursor % MAX_VIS_OPT == 0) {  // page to next
-            //     m_list_options(m, 1);
-            //     m->coffset += MAX_VIS_OPT;
-            //     m_list_options(m, 0);
-            // }
-            break;
-        case CURS_LEFT:
-            // move back level
-            break;
-        case CURS_RIGHT:
-            // move to option target
-            break;
-        case CURS_SELECT:
-            break;
-        default:
-            return 1;
+    tpos.y += 3 * m->opt_div;
+
+    // test for cancel
+    if (TEST_RECT(t.x, t.y, A_COLUMN.x, tpos.y, COLUMN_W, COLUMN_H)) {
+        return 6;
+    } else if (TEST_RECT(t.x, t.y, B_COLUMN.x, tpos.y, COLUMN_W, COLUMN_H)) {
+        return 7;
     }
 
     return 0;
 }
 
-int m_interact_set(menu_t *m, int it) {
-    static int prev_cursor;
+int m_interact(menu_t *m, TSPoint p) {
     if (!m) return -1;
 
-    prev_cursor = m->cursor;
+    return MENU_INTERACT[m->it_flag](m, p);
+}
 
-    switch (it) {
-        case CURS_UP:
-            if (m->cursor < 0) m->cursor = 0;
-            m->options[0].value += pow(10, m->cursor);
-            break;
-        case CURS_DOWN:
-            if (m->cursor < 0) m->cursor = 0;
-            m->options[0].value -= pow(10, m->cursor);
-            break;
-        case CURS_LEFT:
-            if (m->cursor < 0) m->cursor = 0;
-            m->cursor = mod(m->cursor + 1, 4);
-            break;
-        case CURS_RIGHT:
-            if (m->cursor < 0) m->cursor = 0;
-            m->cursor = mod(m->cursor - 1, 4);
-            break;
-        case CURS_SELECT:
-            m->cursor = -1;
-            break;
-        default:
-            return 1;
+int m_interact_default(menu_t *m, TSPoint p) {
+  int idx = m_test_touch(p, m);
+  if (idx >= 0 && p.z > 70) {
+	return idx;
+  }
+
+  return -1;
+}
+
+int m_interact_set(menu_t *m, TSPoint p) {
+    int vbuf[4] = {
+        m->options[0].value / 1000 % 10,
+        m->options[0].value / 100  % 10,
+        m->options[0].value / 10   % 10,
+        m->options[0].value % 10
+    };
+    int idx = m_test_touch_set(p, m);
+
+    if (idx < 0) {  // decrement
+        vbuf[abs(idx) - 1] = mod(vbuf[abs(idx) - 1] - 1, 10);
+        m->options[0].value = 1000 * vbuf[0] + 100 * vbuf[1] + 10 * vbuf[2] + vbuf[3];
+        return S_NEW_VALUE;
+    } if (idx > 0 && idx < 6) {    // increment
+        vbuf[idx - 1] = mod(vbuf[idx - 1] + 1, 10);
+        m->options[0].value = 1000 * vbuf[0] + 100 * vbuf[1] + 10 * vbuf[2] + vbuf[3];
+        return S_NEW_VALUE;
+    } if (idx == 6) {  // cancel
+        m->cb(m, &m->options[idx]);
+        return idx;
+    } if (idx == 7) {  // confirm
+        // format value
+        m->options[0].value = 1000 * vbuf[0] + 100 * vbuf[1] + 10 * vbuf[2] + vbuf[3];
+        m->cb(m, &m->options[idx]);
+        return idx;
     }
 
-    return 0;
+    return S_NO_CHANGE;
 }
 
 
