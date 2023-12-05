d226ad5ab6114af8f2a71fe68ac811757837f2d3
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-12-04 17:57:04 -0800

    test menus

--- menu/src/main.cpp
+++ menu/src/main.cpp
@@ -15,20 +15,27 @@
 #define YMIN    A1
 #define XMIN    A0
 
-TouchScreen ts = TouchScreen(YPOS, XPOS, YMIN, XMIN, 300);
 #define TFT_CS 37
 #define TFT_DC 36
-Adafruit_ILI9341 tft = Adafruit_ILI9341(tft8bitbus, 22, 35, 36, 37, 33, 34);
 
-#define CENTER (vec2){160,120}
+#define M_SIZE (vec2){320,240}
+
+TouchScreen ts = TouchScreen(YPOS, XPOS, YMIN, XMIN, 300);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(tft8bitbus, 22, 35, 36, 37, 33, 34);
 
 void init_menus();
 void init_input();
 int get_input();
 
-menu_t *main_menu, *presets, *config, *alarms, *pressures, *timers, *purge_timers, *delay_timers, *safety_timers;
-menu_t *pick_preset, *pick_param, *pick_mode, *pick_pid, *pick_circuit;
-menu_t *set_param, *reclaimer_config;
+menu_t *set_param;
+menu_t *main_menu;
+menu_t *mode;
+menu_t *presets;
+menu_t *circuit_select;
+menu_t *reclaimer_config;
+menu_t *pick_param;
+menu_t *pick_pid;
+menu_t *pick_preset;
 
 struct ui { 
 	menu_t *active;
@@ -39,161 +46,69 @@ struct ui {
 } ui;
 
 void create_menus() {
-	set_param = new_menu("SET PARAMETER", 8, CENTER);
-	m_set_size(set_param, 320, 220);
-	m_set_draw(set_param, M_SET);
-	m_set_interact(set_param, M_SET);
-	set_param->cur_color = 0x05A0;
-
-	main_menu = new_menu("MAIN", 8, CENTER);
-	m_set_size(main_menu, 320, 220);
-	m_set_draw(main_menu, M_DEFAULT);
-	main_menu->cur_color = 0x05A0;
-
-	timers = new_menu("TIMERS CONFIG", 8, CENTER);
-	m_set_size(timers, 320, 220);
-	m_set_draw(timers, M_DEFAULT);
-	timers->cur_color = 0x05A0;
-
-	presets = new_menu("PRESETS", 8, CENTER);
-	m_set_size(presets, 320, 220);
-	m_set_draw(presets, M_DEFAULT);
-	presets->cur_color = 0x05A0;
-
-	delay_timers = new_menu("SET CIRCUIT DELAY", 8, CENTER);
-	m_set_size(delay_timers, 320, 220);
-	m_set_draw(delay_timers, M_DEFAULT);
-	delay_timers->cur_color = 0x05A0;
-
-	purge_timers = new_menu("SET PURGE TIMES", 8, CENTER);
-	m_set_size(purge_timers, 320, 220);
-	m_set_draw(purge_timers, M_DEFAULT);
-	purge_timers->cur_color = 0x05A0;
-
-	alarms = new_menu("ALARM CONFIG", 8, CENTER);
-	m_set_size(alarms, 320, 220);
-	m_set_draw(alarms, M_DEFAULT);
-	alarms->cur_color = 0x05A0;
-
-	reclaimer_config = new_menu("RECLAIMER CONFIG", 8, CENTER);
-	m_set_size(reclaimer_config, 320, 220);
-	m_set_draw(reclaimer_config, M_DEFAULT);
-	reclaimer_config->cur_color = 0x05A0;
-
-	pressures = new_menu("SET PRESSURES", 8, CENTER);
-	m_set_size(pressures, 320, 220);
-	m_set_draw(pressures, M_DEFAULT);
-	pressures->cur_color = 0x05A0;
-
-	pick_pid = new_menu("PID CONFIG", 8, CENTER);
-	m_set_size(pick_pid, 320, 220);
-	m_set_draw(pick_pid, M_DEFAULT);
-	pick_pid->cur_color = 0x05A0;
+	set_param = new_menu("SET PARAMETER", 1, CENTER, M_SIZE, M_SET);
+	main_menu = new_menu("MAIN", 3, CENTER, M_SIZE, M_DEFAULT);
+	mode = new_menu("MODE SELECT", 3, CENTER, M_SIZE, M_DEFAULT);
+	presets = new_menu("PRESETS", 3, CENTER, M_SIZE, M_DEFAULT);
+	circuit_select = new_menu("PICK CIRCUIT", 7, CENTER, M_SIZE, M_DEFAULT);
+	reclaimer_config = new_menu("RECLAIMER CONFIG", 4, CENTER, M_SIZE, M_DEFAULT);
+	pick_param = new_menu("PICK PARAM.", 5, CENTER, M_SIZE, DEFAULT);
+	pick_pid = new_menu("PID CONFIG", 3, CENTER, M_SIZE, M_DEFAULT);
+	pick_preset = new_menu("PICK PRESET", 4, CENTER, M_SIZE, M_PRINT);
 }
 
 void init_options() {
 	option_t opts[8];
 
-	opts[0] = (option_t) {"0000", 		NULL,    1234};	// SUBMENU (PRESETS MENU)
-	opts[1] = (option_t) {"", 			NULL, 	 0};	// SUBMENU (TIMERS MENU)
-	opts[2] = (option_t) {"",  			NULL,  	 0};	// SUBMENU (PID MENU)
-	opts[3] = (option_t) {"", 			NULL,    0};	// SUBMENU (PRESSURES MENU)
-	opts[4] = (option_t) {"", 			NULL,    0};	// SUBMENU (ALARM CONFIG)
-	opts[5] = (option_t) {"", 			NULL,    0};	// SUBMENU (RECLAIMER MENU)
-	opts[6] = (option_t) {"CANCEL", 	NULL,    0};	// Not necessary on MAIN
-	opts[7] = (option_t) {"CONFIRM", 	NULL,    0};	// Not necessary on MAIN
+	opts[0] = (option_t) {"", 		NULL,    1234};
 	m_set_options(set_param, set_param->nopts, opts);
 	
-	opts[0] = (option_t) {"Presets", 	presets,    0};	//SUBMENU (PRESETS MENU)
-	opts[1] = (option_t) {"Times", 		timers, 	 0};	//SUBMENU (TIMERS MENU)
-	opts[2] = (option_t) {"PID",  		pick_pid,  	 0};	//SUBMENU (PID MENU)
-	opts[3] = (option_t) {"Pressures", 	pressures,    0};	//SUBMENU (PRESSURES MENU)
-	opts[4] = (option_t) {"Alarms", 	alarms,    0};	//SUBMENU (ALARM CONFIG)
-	opts[5] = (option_t) {"Reclaimer", 	reclaimer_config,    0};	//SUBMENU (RECLAIMER MENU)
-	opts[6] = (option_t) {"", 		NULL,    0};	//Not necessary on MAIN
-	opts[7] = (option_t) {"", 		NULL,    0};	//Not necessary on MAIN
+	opts[0] = (option_t) {"Mode", 	mode,     0};
+	opts[1] = (option_t) {"Preset", presets,  0};
+	opts[2] = (option_t) {"Config", pick_pid, 0};
 	m_set_options(main_menu, main_menu->nopts, opts);
 
-	opts[0] = (option_t) {"Save", 	NULL,    0};	//SUBMENU, FUNCTION
-	opts[1] = (option_t) {"Load", 	NULL, 	 0};	//SUBMENU, FUNCTION
-	opts[2] = (option_t) {"Delete",  NULL,  	 0};	//SUBMENU, FUNCTION
-	opts[3] = (option_t) {"", 				NULL,    0};
-	opts[4] = (option_t) {"", 				NULL,    0};
-	opts[5] = (option_t) {"", 				NULL,    0};
-	opts[6] = (option_t) {"BACK", 		main_menu,    0};	//To MAIN
-	opts[7] = (option_t) {"EXIT", 		main_menu,    0};	//To MAIN
-	
-	m_set_options(presets, presets->nopts, opts);	
+	opts[0] = (option_t) {"Auto", 	 main_menu,    0};
+	opts[1] = (option_t) {"Manual",	 main_menu,    0};
+	opts[2] = (option_t) {"Standby", main_menu,    0};
+	m_set_options(mode, mode->nopts, opts);
 
-	opts[0] = (option_t) {"MARX", 		NULL,    0};	//FUNCTION
-	opts[1] = (option_t) {"MARXTG70", 	NULL, 	 0};	//FUNCTION
-	opts[2] = (option_t) {"MTG",  		NULL,  	 0};	//FUNCTION
-	opts[3] = (option_t) {"SWITCH", 	NULL,    0};	//FUNCTION
-	opts[4] = (option_t) {"SWTG70",     NULL,    0};	//FUNCTION
-	opts[5] = (option_t) {"", 		    NULL,    0};
-	opts[7] = (option_t) {"BACK", 		main_menu,    0};	//To MAIN
-	opts[7] = (option_t) {"EXIT", 		main_menu,    0};	//To MAIN
-	m_set_options(pressures, pressures->nopts, opts);
+	opts[0] = (option_t) {"Save", 	pick_preset, 0};
+	opts[1] = (option_t) {"Load", 	pick_preset, 0};
+	opts[2] = (option_t) {"Delete", pick_preset, 0};
+	m_set_options(presets, presets->nopts, opts);
+
+	// can create a circuit -> value map for each circuit
+	opts[0] = (option_t) {"MARX", 		pick_param, 0};
+	opts[1] = (option_t) {"MX-TG70", 	pick_param, 0};
+	opts[2] = (option_t) {"MTG", 		pick_param, 0};
+	opts[3] = (option_t) {"SWITCH", 	pick_param, 0};
+	opts[4] = (option_t) {"SW-TG70", 	pick_param, 0};
+	opts[5] = (option_t) {"RECLAIM", 	set_param, 	0};		// only has configurable pressure
+	opts[6] = (option_t) {"MIN SUPL", 	set_param, 	0};		// only has configurable pressure
+	m_set_options(circuit_select, circuit_select->nopts, opts);
+
+	// could potentially swap these two in the hierarchy
+	// depending on whether it's more common to modify more params for a single circuit
+	// or the same param for multiple circuits
+	
+	opts[0] = (option_t) {"Pressure", 	set_param, 0};
+	opts[1] = (option_t) {"Purge", 		set_param, 0};
+	opts[2] = (option_t) {"Delay", 		set_param, 0};
+	opts[3] = (option_t) {"Alarm", 		set_param, 0};
+	opts[4] = (option_t) {"PID", 		pick_pid,  0};
+	m_set_options(pick_param, pick_param->nopts, opts);
+
+	opts[0] = (option_t) {"KP", set_param, 		0};
+	opts[1] = (option_t) {"KI", set_param, 		0};
+	opts[2] = (option_t) {"KD",	set_param, 		0};
+	m_set_options(pick_pid, pick_pid->nopts, opts);
 
 	opts[0] = (option_t) {"REC ON", 	NULL,    0};	//FUNCTION
 	opts[1] = (option_t) {"REC OFF", 	NULL,    0};	//FUNCTION
 	opts[2] = (option_t) {"MIN SPLY", 	NULL,    0};	//FUNCTION
-	opts[3] = (option_t) {"Safe Delay",   NULL,    0};	//FUNCTION
-	opts[4] = (option_t) {"", 				NULL,    0};
-	opts[7] = (option_t) {"BACK", 		main_menu,    0};	//To MAIN
-	opts[7] = (option_t) {"EXIT", 		main_menu,    0};	//To MAIN
+	opts[3] = (option_t) {"Safe Delay", NULL,    0};	//FUNCTION
 	m_set_options(reclaimer_config, reclaimer_config->nopts, opts);
-
-	opts[0] = (option_t) {"Sound ON/OFF", 	NULL,    0};	//FUNCTION
-	opts[1] = (option_t) {"MARX", 	NULL,    0};	//FUNCTION
-	opts[2] = (option_t) {"MARXTG70", NULL, 	 0};	//FUNCTION
-	opts[3] = (option_t) {"MTG",  	NULL,  	 0};	//FUNCTION
-	opts[4] = (option_t) {"SWITCH", 	NULL,    0};	//FUNCTION
-	opts[5] = (option_t) {"SWTG70", NULL,    0};	//FUNCTION
-	opts[6] = (option_t) {"BACK", 		main_menu,    0};	//To MAIN
-	opts[7] = (option_t) {"EXIT", 		main_menu,    0};	//To MAIN
-	
-	m_set_options(alarms, alarms->nopts, opts);
-
-	opts[0] = (option_t) {"Purge", purge_timers,   0};	//SUBMENU (TIMERS MENU)
-	opts[1] = (option_t) {"Delay", delay_timers, 0};	//SUBMENU (TIMERS MENU)
-	opts[2] = (option_t) {"", 		NULL,  	 0};
-	opts[3] = (option_t) {"", 		NULL,    0};
-	opts[4] = (option_t) {"", 		NULL,    0};
-	opts[5] = (option_t) {"", 		NULL,    0};
-	opts[6] = (option_t) {"BACK", 		main_menu,    0};	//To MAIN
-	opts[7] = (option_t) {"EXIT", 		main_menu,    0};	//To MAIN
-	m_set_options(timers, timers->nopts, opts);
-
-	opts[0] = (option_t) {"MARX", 	set_param,    0};	//FUNCTION
-	opts[1] = (option_t) {"MARXTG70", NULL, 	 0};	//FUNCTION
-	opts[2] = (option_t) {"MTG",  	NULL,  	 0};	//FUNCTION
-	opts[3] = (option_t) {"SWITCH", 	NULL,    0};	//FUNCTION
-	opts[4] = (option_t) {"SWTG70", NULL,    0};	//FUNCTION
-	opts[5] = (option_t) {"",		NULL,    0};
-	opts[6] = (option_t) {"BACK", 		timers,    0};	//To timers
-	opts[7] = (option_t) {"EXIT", 		main_menu,    0};	//To MAIN
-	m_set_options(purge_timers, purge_timers->nopts, opts);	
-
-	opts[0] = (option_t) {"MARX", 	NULL,    0};	//FUNCTION
-	opts[1] = (option_t) {"MARXTG70", NULL, 	 0};	//FUNCTION
-	opts[2] = (option_t) {"MTG",  	NULL,  	 0};	//FUNCTION
-	opts[3] = (option_t) {"SWITCH", 	NULL,    0};	//FUNCTION
-	opts[4] = (option_t) {"SWTG70", NULL,    0};	//FUNCTION
-	opts[5] = (option_t) {"",		NULL,    0};
-	opts[6] = (option_t) {"BACK", 		timers,    0};	//To TIMERS 
-	opts[7] = (option_t) {"EXIT", 		main_menu,    0};	//To MAIN
-	m_set_options(delay_timers, delay_timers->nopts, opts);
-
-	opts[0] = (option_t) {"MARX", 		 NULL,    0};	//FUNCTION
-	opts[1] = (option_t) {"MARXTG70",	 NULL, 	 0};	//FUNCTION
-	opts[2] = (option_t) {"MTG",  		 NULL,  	 0};	//FUNCTION
-	opts[3] = (option_t) {"SWITCH", 	 NULL,    0};	//FUNCTION
-	opts[4] = (option_t) {"SWTG70", NULL,    0};	//FUNCTION
-	opts[5] = (option_t) {"", 				 NULL,    0};
-	opts[6] = (option_t) {"BACK", 			 main_menu,    0};	//To MAIN
-	opts[7] = (option_t) {"EXIT", 			 main_menu,    0};	//To MAIN
-	m_set_options(pick_pid, pick_pid->nopts, opts);	
 }
 
 void init_menus() {
