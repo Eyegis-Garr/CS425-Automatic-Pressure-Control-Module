f808926079888c0f24700ca5850cf7fd637aa0b9
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2023-12-04 22:09:45 -0800

    added alert interactions. other minor changes
    implemented touch-testing against alert menus. changed default interact routine to use fancy new menu codes

--- menu/lib/menu/menu_interact.cpp
+++ menu/lib/menu/menu_interact.cpp
@@ -1,28 +1,37 @@
 #include "menu.h"
 
-MenuInteract MENU_INTERACT[4] = {
+MenuInteract MENU_INTERACT[NUM_MTYPES] = {
     m_interact_default,
     m_interact_set,
     NULL,
-    NULL
+    NULL,
+    NULL,
+    m_interact_message
 };
 
 int m_test_touch(TSPoint t, menu_t *m) {
-    if (!m) return -1;
+    if (!m) return M_NOP;
     vec2 tpos = (vec2) { m->center.x, m->center.y - m->opt_offset - m->opt_div + 80 };
 
     for (int i = 0; i < m->nopts - 1; i += 2) {
         if (TEST_RECT(t.x, t.y, A_COLUMN.x, tpos.y, COLUMN_W, COLUMN_H)) {        // CURRENT A OPTION
-            return i;
-        } else if (TEST_RECT(t.x, t.y, B_COLUMN.x, tpos.y, COLUMN_W, COLUMN_H)) { // CURRENT B OPTION
-            
-            return i + 1;
+            m->cursor = i;
+            return M_SELECT;
+        } else if (i + 1 < m->nopts && TEST_RECT(t.x, t.y, B_COLUMN.x, tpos.y, COLUMN_W, COLUMN_H)) { // CURRENT B OPTION
+            m->cursor = i + 1;
+            return M_SELECT;
         }
 
         tpos.y += m->opt_div;
     }
 
-    return -1;
+    if (TEST_RECT(t.x, t.y, A_BTN.x, A_BTN.y, COLUMN_W, COLUMN_H)) {   
+        return M_BACK;
+    } else if (TEST_RECT(t.x, t.y, B_BTN.x, B_BTN.y, COLUMN_W, COLUMN_H)) {
+        return M_EXIT;
+    }
+
+    return M_NOP;
 }
 
 int m_test_touch_set(TSPoint t, menu_t *m) {
@@ -54,6 +63,18 @@ int m_test_touch_set(TSPoint t, menu_t *m) {
     return M_NOP;
 }
 
+int m_test_touch_msg(TSPoint t, menu_t *m) {
+    if (!m) return M_NOP;
+
+    if (TEST_RECT(t.x, t.y, A_BTN.x, A_BTN.y - 100, COLUMN_W, COLUMN_H)) {
+        return M_CONFIRM;
+    } else if (TEST_RECT(t.x, t.y, B_BTN.x, B_BTN.y - 100, COLUMN_W, COLUMN_H)) {
+        return M_BACK;
+    }
+
+    return M_NOP;
+}
+
 int m_interact(menu_t *m, TSPoint p) {
     if (!m) return M_NOP;
 
@@ -63,22 +84,25 @@ int m_interact(menu_t *m, TSPoint p) {
 int m_interact(menu_t *m, int command) {
     if (!m) return M_NOP;
 
-    
+
+    return M_NOP;
+}
+
+int m_interact_message(menu_t *m, TSPoint p) {
+    return m_test_touch_msg(p, m);
 }
 
 int m_interact_default(menu_t *m, TSPoint p) {
-  int idx = m_test_touch(p, m);
-  if (idx >= 0 && p.z > 70) {
-    m->cursor = idx;
+  int code = m_test_touch(p, m);
+  if (code == M_SELECT) {
     if (m->cb) m->cb(m, &m->options[m->cursor]);
-	return M_SELECT;
   }
 
-  return M_NOP;
+  return code;
 }
 
 int m_interact_set(menu_t *m, TSPoint p) {
-    int idx = m_test_touch_set(p, m), d;
+    int code = m_test_touch_set(p, m), d;
     int vbuf[4] = {
         m->options[0].value / 1000 % 10,
         m->options[0].value / 100  % 10,
@@ -86,15 +110,15 @@ int m_interact_set(menu_t *m, TSPoint p) {
         m->options[0].value % 10
     };
 
-    if (idx == M_UPDATED) {
+    if (code == M_UPDATED) {
         d = (m->cursor > 0) ? 1 : -1;
         vbuf[abs(m->cursor) - 1] = mod(vbuf[abs(m->cursor) - 1] + d, 10);
         m->options[0].value = 1000 * vbuf[0] + 100 * vbuf[1] + 10 * vbuf[2] + vbuf[3];
         return M_UPDATED;
-    } if (idx == M_BACK) {  // cancel
+    } if (code == M_BACK) {  // cancel
         return M_BACK;
-    } if (idx == M_CONFIRM) {  // confirm
-        if (m->cb) m->cb(m, &m->options[idx]);
+    } if (code == M_CONFIRM) {  // confirm
+        if (m->cb) m->cb(m, &m->options[code]);
         return M_CONFIRM;
     }
 
