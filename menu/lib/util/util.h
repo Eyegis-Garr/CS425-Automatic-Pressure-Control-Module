42b8a4f307bf3c80f34ac35f0fc4efefb8558c21
Bradley Sullivan <bradleysullivan@nevada.unr.edu>
2024-02-05 16:04:16 -0800

    cleanup

--- menu/lib/util/util.h
+++ menu/lib/util/util.h
@@ -3,11 +3,6 @@
 
 #include <inttypes.h>
 
-void msb_radixsort_index(uint16_t *data, uint16_t *idx, int zbin, int obin, uint16_t mask);
-void msb_radixsort(uint16_t *data, int zbin, int obin, uint16_t mask);
-void insertion_sort_index(int *data, int *idx, uint16_t len);
-void insertion_sort(int *data, uint16_t len);
-
 int lerp(int x, int x0, int y0, int x1, int y1);
 int mod(int a, int m);
 int in_range(int a, int b, int v);
