35ab9373b3aaca3a90b8682339a0629a7ef8ff4b
Rolandelli-Sean <70545334+Rolandelli-Sean@users.noreply.github.com>
2024-03-07 10:18:16 -0800

    Updated to include error message

--- testing_gui_to_arduino.ino
+++ testing_gui_to_arduino.ino
@@ -1,46 +1,32 @@
-//must add easy nextion library
-
 #include "EasyNextionLibrary.h"
 
 EasyNex myNex(Serial1);
 
 void setup() {
-
   Serial.begin(9600);
-
+  Serial1.begin(9600);
+  
   myNex.begin(9600);
 
-  pinMode(LED_BUILTIN, OUTPUT);
+  delay(3000);
+  
+  Serial1.print("page 16");
+  Serial1.write(0xff);
+  Serial1.write(0xff);
+  Serial1.write(0xff);
 
+  delay(3000);
+
+  Serial1.print("page 1");
+  Serial1.write(0xff);
+  Serial1.write(0xff);
+  Serial1.write(0xff);
 }
 
 void loop() {
-
   myNex.NextionListen();
-
 }
 
 void trigger0() {
   Serial.println("hi");
 }
-
-
-/* old way of getting info from screen
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial1.begin(9600);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  String dfd = ""; //data from display
-
-  while(Serial1.available() > 0) {
-    dfd += int(Serial1.read());
-  }
-
-  Serial.println(dfd);
-  delay(2000);
-}
-*/
