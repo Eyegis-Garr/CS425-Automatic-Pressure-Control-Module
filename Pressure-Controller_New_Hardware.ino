85cdd2334ec351fc14b054955e5171271e1d5fae
Sean Rolandelli <srolandelli@nevada.unr.edu>
2024-03-21 09:47:45 -0700

    added checksum and getcurrentsettings functions

--- Pressure-Controller_New_Hardware.ino
+++ Pressure-Controller_New_Hardware.ino
@@ -3131,6 +3131,184 @@ void alarmController(String errorString)
 }
 
 
+//-------------------------------------------------------------------------------------------
+//Checksum
+//-------------------------------------------------------------------------------------------
+
+void *getOldSettings(char *buf[]) {
+   
+  buf[0] = "hi";
+  buf[1] = "hello";
+  
+}
+
+void *getCurrentSettings(char *buf[]) {
+   
+  alarmEnable = previousSettingFile.readStringUntil('\n').toInt();
+	currentSettings[0] = alarmEnable;
+
+  isCouple = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[1] = isCouple;
+
+	Marxsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+	currentSettings[2] = Marxsetpoint;
+    
+	MTGsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+	currentSettings[3] = MTGsetpoint;
+    
+	Switchsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+	currentSettings[4] = Switchsetpoint;
+    
+	TG70Switchsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+	currentSettings[5] = TG70Switchsetpoint;
+    
+	TG70Marxsetpoint = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[6] = TG70Marxsetpoint;
+
+	maxReclaimerPressure = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[7] = maxReclaimerPressure;
+
+	minReclaimerPressure = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[8] = minReclaimerPressure;
+
+	marxenableState = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[9] = marxenableState;
+	
+	mtgenableState = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[10] = mtgenableState;
+
+	switchenableState = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[11] = switchenableState;
+
+	tg70switchenableState = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[12] = tg70switchenableState;
+
+	tg70marxenableState = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[13] = tg70marxenableState;
+
+	marxmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[14] = marxmaxTime;
+ 
+	mtgmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[15] = mtgmaxTime;
+
+	switchmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[16] = switchmaxTime;
+
+	tg70switchmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[17] = tg70switchmaxTime
+
+	tg70marxmaxTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[18] = tg70marxmaxTime
+
+	marxDelay = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[19] = marxDelay
+
+	mtgDelay = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[20] = mtgDelay
+
+	switchDelay = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[21] = switchDelay
+
+	tg70marxDelay = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[22] = tg70marxDelay;
+
+	tg70switchDelay = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[23] = tg70switchDelay;
+
+	marxPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[24] = marxPurgeTime;
+
+	mtgPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[25] = mtgPurgeTime;
+
+	switchPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[26] = switchPurgeTime;
+
+	tg70switchPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[27] = tg70switchPurgeTime;
+
+	tg70marxPurgeTime = previousSettingFile.readStringUntil('\n').toInt();
+  currentSettings[28] = tg70marxPurgeTime;
+
+	minBottlePressure = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[29] = minBottlePressure;
+
+	kp_Marx = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[30] = kp_Marx;
+
+	ki_Marx = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[31] = ki_Marx;
+
+	kd_Marx = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[32] = kd_Marx;
+
+	kp_MTG = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[33] = kp_MTG;
+
+	ki_MTG = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[34] = ki_MTG;
+
+	kd_MTG = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[35] = kd_MTG;
+
+	kp_Switch = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[36] = kp_Switch;
+
+	ki_Switch = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[37] = ki_Switch;
+
+	kd_Switch = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[38] = kd_Switch;
+
+	kp_SwitchTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[39] = kp_SwitchTG70
+
+	ki_SwitchTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[40] = ki_SwitchTG70;
+
+	kd_SwitchTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[41] = kd_SwitchTG70;
+
+	kp_MarxTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[42] = kp_MarxTG70;
+
+	ki_MarxTG70 = previousSettingFile.readStringUntil('\n').toDouble();
+  currentSettings[43] = ki_MarxTG70;
+
+	kd_MarxTG70 = previousSettingFile.readStringUntil('\n').toDouble();	
+	currentSettings[44] = kd_MarxTG70;
+  
+}
+
+void Checksum()
+{
+
+  Serial.println("");
+
+  char *oldSettings[2], *currentSettings[2];
+
+  getOldSettings(oldSettings);
+  getCurrentSettings(currentSettings);
+
+  crc.reset();
+
+  int old = 0, current = 0, i = 0;
+
+  while(i < sizeof(oldSettings)/sizeof(int)) {
+    old = calcCRC8((uint8_t *)oldSettings[i], 9);
+    current = calcCRC8((uint8_t *)currentSettings[i], 9);
+    i++;
+
+    if(old != current) {
+      Serial.println("Error");
+      break;
+    }
+  }
+}
+
+
+
 //--------------------------------------------------------------------------------------------
 //TRIGGERS
 //--------------------------------------------------------------------------------------------
